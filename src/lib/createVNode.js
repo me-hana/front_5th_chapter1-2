33735fec8188676ea4484d8dcf8d255f9f360212
me-hana <hayonleee@gmail.com>
2025-04-01 22:39:28 +0900

    feat: createVNode 함수 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,11 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  // Null과 undefined는 어떻게 처리할지 생각
+  // children에 filter를 걸어서 null이거나 undefined가 되는 경우 리턴을 제어
+  return {
+    type,
+    props,
+    children: children
+      .flat(Infinity)
+      .filter((ch) => ch !== null && ch !== undefined),
+  };
 }
