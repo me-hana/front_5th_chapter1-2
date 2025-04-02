990ddaf63a163c11e70eed1f5b8d0f826c2f7f7c
me-hana <hayonleee@gmail.com>
2025-04-02 14:53:22 +0900

    feat: normalizeVNode 구현

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,30 @@
 export function normalizeVNode(vNode) {
-  return vNode;
+  console.log(vNode);
+  if (vNode == null || typeof vNode === "boolean") {
+    return "";
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return vNode.toString();
+  }
+
+  // 함수형 컴포넌트의 경우 함수를 실행시킨다
+  // 단, 실행할 때 props 전달해줘야 함
+  if (typeof vNode.type === "function") {
+    const childComponent = vNode.type({
+      ...vNode.props,
+      children: vNode.children,
+    });
+
+    return normalizeVNode(childComponent);
+  }
+
+  // 함수가 아닌 요소
+  return {
+    type: vNode.type,
+    props: vNode.props,
+    children: Array.isArray(vNode.children)
+      ? vNode.children.map(normalizeVNode).filter((node) => node != "") // falsy 처리
+      : vNode.children,
+  };
 }
