9565d254057827272f1e92c7c6dbb7a95a072b01
me-hana <hayonleee@gmail.com>
2025-04-02 16:17:25 +0900

    feat: createElement 구현

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,35 @@
-import { addEvent } from "./eventManager";
+// import { addEvent } from "./eventManager";
 
-export function createElement(vNode) {}
+export function createElement(vNode) {
+  console.log(vNode);
 
-function updateAttributes($el, props) {}
+  if (vNode == null || typeof vNode === "boolean") {
+    return document.createTextNode("");
+  }
+
+  if (typeof vNode === "string" || typeof vNode === "number") {
+    return document.createTextNode(vNode);
+  }
+
+  if (Array.isArray(vNode)) {
+    const docFrag = document.createDocumentFragment();
+    docFrag.append(...vNode.map(createElement));
+    return docFrag;
+  }
+
+  const el = document.createElement(vNode.type);
+  vNode.children.map(createElement).forEach((child) => el.appendChild(child));
+
+  updateAttributes(el, vNode.props);
+  return el;
+}
+
+function updateAttributes($el, props) {
+  Object.entries(props || {}).map(([attr, value]) => {
+    if (attr === "className") {
+      $el.setAttribute("class", value);
+    } else {
+      $el.setAttribute(attr, value);
+    }
+  });
+}
