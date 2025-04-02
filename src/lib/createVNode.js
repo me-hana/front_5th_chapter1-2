749cbc283e3f42d40e958e4aea1cbdff20b75187
me-hana <hayonleee@gmail.com>
2025-04-02 11:14:26 +0900

    fix: createVNode 느슨한 비교를 활용한 null과 undefined 처리

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,11 +1,12 @@
 export function createVNode(type, props, ...children) {
   // Null과 undefined는 어떻게 처리할지 생각
   // children에 filter를 걸어서 null이거나 undefined가 되는 경우 리턴을 제어
+  // 느슨한 비교
   return {
     type,
     props,
     children: children
       .flat(Infinity)
-      .filter((ch) => ch !== null && ch !== false),
+      .filter((ch) => ch != null && ch !== false),
   };
 }
